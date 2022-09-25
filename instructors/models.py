a3fff07dcfbc22db2e555611f85199b1cd7bbe09
unknown <ismailibrahimabdinur143@gmail.com>
2022-09-25 16:33:10 +0300

    Create the instructor model

--- instructors/models.py
+++ instructors/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Instructor(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='instructors')
+    qualification = models.CharField(max_length=300)
+    mobile_phone = models.CharField(max_length=40)
+    about = models.TextField()
+
+    def __str__(self):
+        return self.user.username
