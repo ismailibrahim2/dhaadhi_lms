d3e33b366212dc7ada43438eea17f3cb42f033d3
unknown <ismailibrahimabdinur143@gmail.com>
2022-09-27 17:01:37 +0300

    Creating the Module, Lesson Model

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Category, Course, Module, Lesson
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['category', 'title', 'instructor', 'price', 'status']
+    list_filter = ['category', 'instructor', 'status']
+
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ['course', 'title']
+    list_filter = ['course']
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['module', 'name', 'slug']
+    list_filter = ['module']
--- courses/models.py
+++ courses/models.py
@@ -24,6 +24,14 @@ def save_course_image(instance, filename):
     return os.path.join(upload_to, filename)
 
 
+def save_course_intros(instance, filename):
+    upload_to = 'Course Intros/'
+    ext = filename.split('.')[-1]
+    if instance.title:
+        filename = f'{instance.instructor.user.user}/{instance.category.name}/{instance.title}/{instance.title}.{ext}'
+    return os.path.join(upload_to, filename)
+
+
 class Course(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -34,7 +42,9 @@ class Course(models.Model):
     title = models.CharField(max_length=400)
     slug = models.SlugField(max_length=400, unique=True, blank=True, null=True)
     thumbnail = models.ImageField(upload_to=save_course_image)
+    overview_vid = models.FileField(upload_to=save_course_intros)
     description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
     status = models.CharField(
         max_length=30, choices=STATUS_CHOICES, default='draft')
 
@@ -44,3 +54,33 @@ class Course(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Module(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+
+    def __str__(self):
+        return self.title
+
+
+def save_lesson_videos(instance, filename):
+    upload_to = 'Course Lessons/'
+    ext = filename.split('.')[-1]
+    if instance.name:
+        filename = f'{instance.module.course.instructor.user.user}/{instance.module.course.title}/{instance.module.title}/{instance.name}/{instance.name}.{ext}'
+    return os.path.join(upload_to, filename)
+
+
+class Lesson(models.Model):
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    video = models.FileField(upload_to=save_lesson_videos)
+
+    def __str__(self):
+        return self.name
